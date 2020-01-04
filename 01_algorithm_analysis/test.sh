af1383f93d7a38e79343cfdda9d61c457cd075dd
broliang <18706703437@163.com>
2020-01-04 13:28:43 +0800

    Modify 01_algorithm_analysis/test.sh and new Makefile: Use make for compilation

--- 01_algorithm_analysis/test.sh
+++ 01_algorithm_analysis/test.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
 rm -f a.out
-
-# Enable extended globbing in bash
-shopt -s extglob
-
-get_h !(test).c > test.h
-gcc -I $unity_path $unity_path/unity.c *.c
+make
 ./a.out
-
-# Disable extended globbing in bash
-shopt -u extglob
